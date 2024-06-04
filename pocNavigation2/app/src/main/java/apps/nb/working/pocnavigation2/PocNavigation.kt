1466df0770c7940684b694e3ee7f5405ad516414
Nizar <nizar.belhouchet@gmail.com>
2024-06-04 13:49:39 +0200

    Fonctionnal navigation graph

---
+++ pocNavigation2/app/src/main/java/apps/nb/working/pocnavigation2/PocNavigation.kt
@@ -0,0 +1,12 @@
+package apps.nb.working.pocnavigation2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PocNavigation : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ pocNavigation2/app/src/main/java/apps/nb/working/pocnavigation2/ui/screen/LoginViewModel.kt
@@ -0,0 +1,8 @@
+//package apps.nb.working.pocnavigation2.ui.screen
+//
+//import androidx.lifecycle.ViewModel
+//import dagger.hilt.android.lifecycle.HiltViewModel
+//import javax.inject.Inject
+//
+//@HiltViewModel
+//class LoginViewModel @Inject constructor(): ViewModel()
