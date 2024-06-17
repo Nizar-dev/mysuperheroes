92b7ef5e58728fa4b8b568c13f6fe5c5a3033149
Nizar-dev <54778277+Nizar-dev@users.noreply.github.com>
2024-06-17 16:02:04 +0200

    Delete pocNavigation2 directory
    error of commit between two distinct projects

--- pocNavigation2/app/src/main/java/apps/nb/working/pocnavigation2/ui/screen/HomeViewModel.kt
+++
@@ -1,8 +0,0 @@
-//package apps.nb.working.pocnavigation2.ui.screen
-//
-//import androidx.lifecycle.ViewModel
-//import dagger.hilt.android.lifecycle.HiltViewModel
-//import javax.inject.Inject
-//
-//@HiltViewModel
-//class HomeViewModel @Inject constructor(): ViewModel()
